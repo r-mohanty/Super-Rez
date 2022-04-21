2eee6d86badf5ec6fc112b45f8969a0294a8440b
DustInMyGit <dustin.h.wu@gmail.com>
2022-04-21 17:39:20 -0400

    fixed numerous bad issues

--- Train.py
+++ Train.py
@@ -8,33 +8,62 @@ import logging
 import torch.backends.cudnn as cudnn
 import torch.optim as optim
 import torch.utils.data
-import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
-import copy
+from torch.utils.data import Dataset, DataLoader
 from Network import Generator
 from PIL import Image
 
-class SRDataset(dset.VisionDataset):
-    def __init__(self, root, transform=None, target_transform=None):
-        super().__init__(root, transform=transform, target_transform=target_transform)
+"""
+Expected Dataset File Structure:
+
+DIV2k
+-> DIV2K_train_HR
+    -> HR images
+-> DIV2K_train_LR_bicubic
+    -> X2
+        -> LR images
+
+Download data from here: https://data.vision.ee.ethz.ch/cvl/DIV2K/
+"""
+
+class SRDataset(Dataset):
+    def __init__(self, root, cropSize):
+        self.root = root
         samples = self.make_dataset(self.root)
-        self.loader = loader
+        self.cropSize = cropSize
         self.samples = samples
-        self.targets = [s[1] for s in samples]
     
     def __getitem__(self, index):
         data_path, target_path = self.samples[index]
-        data_img = self.pil_loader(data_path)
+        input_img = self.pil_loader(data_path)
         target_img = self.pil_loader(target_path)
-        if self.transform is not None:
-            data_img = self.transform(data_img)
-        if self.target_transform is not None:
-            target_img = self.target_transform(target_img)
-        return data_img, target_img
+        x, y = self.transform(input_img, target_img)
+        return x, y
 
     def __len__(self):
         return len(self.samples)
+
+    # We put all our transforms here because the input (LR) and target (HR) images must be transformed identically
+    def transform(self, input_img, target_img):
+        # Randomly crop both images (make sure that same corresponding regions are being cropped!!!)
+        i, j, h, w = transforms.RandomCrop.get_params(input_img, output_size=(self.cropSize, self.cropSize))
+        input_img = transforms.functional.crop(input_img, i, j, h, w)
+        target_img = transforms.functional.crop(target_img, i*2, j*2, h*2, w*2)
+
+        # Randomly hor flip both images
+        if random.random() > 0.5:
+            input_img = transforms.functional.hflip(input_img)
+            target_img = transforms.functional.hflip(target_img)
+
+        # Randomly vert flip both images
+        if random.random() > 0.5:
+            input_img = transforms.functional.vflip(input_img)
+            target_img = transforms.functional.vflip(target_img)
+
+        input_img = transforms.functional.to_tensor(input_img)
+        target_img = transforms.functional.to_tensor(target_img)
+        return input_img, target_img
 
     def pil_loader(self, path):
         with open(path, "rb") as f:
@@ -44,10 +73,9 @@ class SRDataset(dset.VisionDataset):
     def make_dataset(self, directory):
         data_folder = os.path.join(directory, "DIV2K_train_LR_bicubic", "X2")
         target_folder = os.path.join(directory, "DIV2K_train_HR")
-        data_paths = [f for f in sorted(os.listdir(data_folder)) if f.endswith(".png")]
-        target_paths = [f for f in sorted(os.listdir(target_folder)) if f.endswith(".png")]
+        data_paths = [os.path.join(data_folder, f) for f in sorted(os.listdir(data_folder)) if f.endswith(".png")]
+        target_paths = [os.path.join(target_folder, f) for f in sorted(os.listdir(target_folder)) if f.endswith(".png")]
         return list(zip(data_paths, target_paths))
-
 
 def main():
     torch.set_printoptions(threshold=1)
@@ -79,17 +107,12 @@ def main():
 
     cudnn.benchmark = True
 
-    dataset = SRDataset('./DIV2K', transform=transforms.Compose([
-        transforms.Resize(opt.imageSize),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-        transforms.RandomHorizontalFlip(p=0.5)
-    ]))
+    dataset = SRDataset('./DIV2K', opt.imageSize)
 
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
+    dataloader = DataLoader(dataset, batch_size=opt.batchSize,
                                             shuffle=True, num_workers=int(opt.workers), drop_last=True)
 
-    device = torch.device("cuda:0")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     
     netG = Generator().to(device)
     optimizerG = optim.Adam(netG.parameters(), lr=opt.lr, betas=(0.9, 0.999))
@@ -101,14 +124,22 @@ def main():
     print('G params: ' + str(sum(p.numel() for p in netG.parameters() if p.requires_grad)))
 
     netG.zero_grad()
-    for epoch in range(0 if model is None else model['epoch'] + 1, 1000):
-        for i, sample in enumerate(dataloader, 0):
+    for epoch in range(0 if model is None else model['epoch'] + 1, 10000):
+        for i, sample in enumerate(dataloader):
             netG.requires_grad = True
             
-            data_img_batch = sample[:, 0, :, :, :]
-            target_img_batch = sample[:, 1, :, :, :]
+            input_img_batch = sample[0]
+            target_img_batch = sample[1]
 
-            output_img_batch = netG(data_img_batch)
+            # Quick test to make sure that we're cropping properly
+            # import matplotlib.pyplot as plt
+            # f, axarr = plt.subplots(1,2)
+            # axarr[0].imshow(input_img_batch[0].permute(1, 2, 0))
+            # axarr[1].imshow(target_img_batch[0].permute(1, 2, 0))
+            # plt.show()
+            # exit()
+
+            output_img_batch = netG(input_img_batch)
 
             loss = torch.nn.L1Loss()
             errG = loss(output_img_batch, target_img_batch)
@@ -128,7 +159,7 @@ def main():
             ###########################
 
             if (i + 1) % 100 == 0:
-                vutils.save_image(data_img_batch, '%s/lr_samples.png' % opt.outf, normalize=True, nrow=3)
+                vutils.save_image(input_img_batch, '%s/lr_samples.png' % opt.outf, normalize=True, nrow=3)
                 vutils.save_image(target_img_batch, '%s/hr_real_samples.png' % opt.outf, normalize=True, nrow=3)
                 vutils.save_image(output_img_batch, '%s/hr_fake_samples.png' % opt.outf, normalize=True, nrow=3)
 
