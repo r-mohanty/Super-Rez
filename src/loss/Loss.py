7d4537325e31ae980caa0b176afaf357ed0ee38f
John Doe <nickgray0@gmail.com>
2022-05-02 01:03:01 -0400

    fidelity loss

--- src/loss/Loss.py
+++ src/loss/Loss.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from MATLAB import imresize
 
 def ZeroCenteredGradientPenalty(Samples, Critics):
     Gradient, = torch.autograd.grad(outputs=Critics.sum(), inputs=Samples, create_graph=True, only_inputs=True)
@@ -7,3 +8,15 @@ def ZeroCenteredGradientPenalty(Samples, Critics):
 
 def RelativisticLoss(PositiveCritics, NegativeCritics):
     return nn.functional.binary_cross_entropy_with_logits(PositiveCritics - NegativeCritics, torch.ones_like(PositiveCritics))
+
+def FidelityLoss(HighResolutionSamples, LowResolutionSamples):
+    Downsampled = imresize(HighResolutionSamples, sizes=(LowResolutionSamples.shape[2], LowResolutionSamples.shape[3]))
+    return nn.functional.l1_loss(Downsampled, LowResolutionSamples)
+
+
+
+
+### quick test ###
+# HighResolutionSamples = torch.randn(10, 3, 192, 192, requires_grad=True)
+# LowResolutionSamples = torch.randn(10, 3, 48, 48)
+# FidelityLoss(HighResolutionSamples, LowResolutionSamples).backward()
